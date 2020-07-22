c66ea53eaed2e4ec8b1d26d51f08182facb7c8d6
Yuan Che <edwarechang7878@hotmail.com>
2020-07-22 19:04:54 +0800

    no message

--- app/src/main/java/com/ed/shuneladmin/MainActivity.java
+++ app/src/main/java/com/ed/shuneladmin/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ed.shuneladmin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //建立bottom
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.homeFragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
     }
 }
