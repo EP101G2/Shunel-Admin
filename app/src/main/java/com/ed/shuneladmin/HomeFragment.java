378e9c127fc8cb80dda10adfb8c062b6b518dc6c
lin3253job <linlin3253job@gmail.com>
2020-08-09 16:35:42 +0800

    no message

--- app/src/main/java/com/ed/shuneladmin/HomeFragment.java
+++ app/src/main/java/com/ed/shuneladmin/HomeFragment.java
@@ -37,35 +37,13 @@ import com.google.android.material.tabs.TabLayoutMediator;
 public class HomeFragment extends Fragment {
 
     private Activity activity;
-
+    private ImageView ivLogout, ivAdmin;
     /*建立viewPage 7.22*/
     TabLayout tabLayout;
     ViewPager2 viewPager;
-<<<<<<< HEAD
     private int[] label={R.string.Promotion,R.string.system,R.string.chat};
-=======
-//<<<<<<< HEAD
-//<<<<<<< HEAD
-    private int[] label={R.string.system,R.string.Promotion,R.string.chat};
-//=======
-//
-//    private int[] label={R.string.system,R.string.Product,R.string.Promotion,R.string.msg,R.string.notice};
-    private ImageView ivLogout, ivAdmin;
-//
-//>>>>>>> d10b0c31dd01221f3d499117e1b7cadfbaa888ef
-//=======
-//    private int[] label={R.string.system,R.string.Promotion,R.string.chat};
->>>>>>> 406d37e89c190569c0bd23f54e2a0edfe238051e
 
 
-//    private ImageView ivLogout;
-
-//>>>>>>> 596b02847cb064046f97b2c5033dd5f892efa7ea
-
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -84,24 +62,17 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-<<<<<<< HEAD
-//        ivLogout = view.findViewById(R.id.ivLogout);
-//        ivAdmin = view.findViewById(R.id.ivAdmin);
-//
-//        ivLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Logout();
-//            }
-//        });
-//        ivAdmin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_adminFragment);
-//            }
-//        });
-=======
+
         ivLogout = view.findViewById(R.id.ivLogout);
+        ivAdmin = view.findViewById(R.id.ivAdim);
+
+
+        ivAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_adminFragment);
+            }
+        });
 
 
         ivLogout.setOnClickListener(new View.OnClickListener() {
@@ -110,7 +81,6 @@ public class HomeFragment extends Fragment {
                 Logout();
             }
         });
->>>>>>> 406d37e89c190569c0bd23f54e2a0edfe238051e
 
 
         findViews(view);
